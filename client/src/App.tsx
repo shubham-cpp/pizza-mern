3cdc46d27bbfa623b89f93dbfdd5837db48618a9
Shubham Pawar <shubhampawar3007@gmail.com>
2021-12-22 22:27:49 +0530

    cleaned out bloat and setup minimal client

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+const App = function (): ReactElement {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
