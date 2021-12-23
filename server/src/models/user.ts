61d311672b64c157a96dab424017057a02f929ec
Shubham Pawar <shubhampawar3007@gmail.com>
2021-12-23 17:00:26 +0530

    added email,password and isadmin

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -3,7 +3,10 @@ import { Schema, model } from 'mongoose';
 interface User {
   name: string;
   age: number;
+  email: string;
   phone: number;
+  password: string;
+  isAdmin: boolean;
 }
 
 const userSchema = new Schema<User>(
@@ -20,6 +23,9 @@ const userSchema = new Schema<User>(
       max: 100,
     },
     phone: { type: Number, required: true, unique: true },
+    password: { type: String, required: true },
+    email: { type: String, unique: true },
+    isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
