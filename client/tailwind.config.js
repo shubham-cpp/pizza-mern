13d5c9c6971b4613c352ea785f2aae6c6cbfe612
Shubham Pawar <shubhampawar3007@gmail.com>
2021-12-24 15:31:13 +0530

    added custom fonts(pushter,source code pro)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,18 @@
 /* eslint-disable */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pushter: ['Pushster', 'cursive'],
+        'source-code': ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        'pizza-bg': "url('./img/pizza-bg.jpg')",
+      },
+    },
   },
   plugins: [],
 };
